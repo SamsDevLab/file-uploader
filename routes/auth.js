59483ce175227bc6afb72e1d159e39b98dbf9775
Sam Hadfield <sthtech@protonmail.com>
2026-07-13 13:28:25 -0500

    refactor: adjust controller import names

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/auth");
+const authController = require("../controllers/authController");
 const validateUserSignUp = require("../middleware/validators");
 
 router.get("/login", authController.renderLoginForm);
