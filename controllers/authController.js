9c9868b16218e0a09732a35ce14a83ba7441e4e8
Sam Hadfield <sthtech@protonmail.com>
2026-07-14 09:53:53 -0500

    feat: route new user data, add new user to db
    Route new user data through authController upon submitting signup form.
    Add new user to Database through model and return user id to add to session
    and login user automatically upon signing up.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const userModel = require("../models/userModel");
+
 async function renderLoginForm(req, res) {
   res.render("auth/login");
 }
@@ -6,8 +8,17 @@ async function renderSignupForm(req, res) {
   res.render("auth/signup");
 }
 
-async function addUserToDb(req, res) {
-  // console.log(req.body);
+async function addUserToDb(req, res, next) {
+  const { passwordConfirmation, ...userData } = req.body;
+  const newUserData = userData;
+  const newUserId = await userModel.addNewUser(newUserData);
+
+  // req.login(newUserId.id, function (err) {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   return res.redirect("/");
+  // });
 }
 
 module.exports = { renderLoginForm, renderSignupForm, addUserToDb };
--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,18 @@
 const bcrypt = require("bcryptjs");
 const prisma = require("../lib/prisma");
 
-async function addNewUser(newUser) {
-  const { email, password } = newUser;
+async function addNewUser(newUserData) {
+  const { email, password } = newUserData;
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
+
+  const newUser = await prisma.user.create({
+    data: {
+      email: email,
+      password: hashedPassword,
+    },
+  });
+
+  return newUser.id;
 }
 
 module.exports = { addNewUser };
