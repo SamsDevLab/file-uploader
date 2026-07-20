7d0b43599f24975c8c74bbcdf895374be87a5c57
Sam Hadfield <sthtech@protonmail.com>
2026-07-20 15:47:10 -0500

    feat: add multer middleware, test file upload
    Add multer middleware to dashboard route. Test with file upload sent to
    the public/uploads directory. Eventually, these uploads will be hosted online

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -2,7 +2,17 @@ const express = require("express");
 const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
 const requireAuth = require("../middleware/requireAuth");
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads/" });
 
 router.get("/", requireAuth, dashboardController.renderDashboard);
+
+router.post(
+  "/upload-file",
+  upload.single("uploadedFile"),
+  function (req, res, next) {
+    console.log(req.file);
+  },
+);
 
 module.exports = router;
