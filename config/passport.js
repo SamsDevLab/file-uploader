10aa938944035cbeeba866ecc3ba3ed39df58831
Sam Hadfield <sthtech@protonmail.com>
2026-07-20 08:25:02 -0500

    refactor: add error messages back to done fns

--- config/passport.js
+++ config/passport.js
@@ -14,12 +14,12 @@ module.exports = function (passport) {
           });
 
           if (!user) {
-            return done(null, false); //{ message: "Incorrect email" }
+            return done(null, false, { message: "Incorrect email" });
           }
           const match = await bcrypt.compare(password, user.password);
 
           if (!match) {
-            return done(null, false); // { message: "Incorrect password" }
+            return done(null, false, { message: "Incorrect password" });
           }
 
           return done(null, user.id);
