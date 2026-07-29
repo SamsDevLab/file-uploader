a6e44d4d0370e4889e8b9a631ba1a73cad7bfdcf
Sam Hadfield <sthtech@protonmail.com>
2026-07-29 13:09:25 -0500

    feat: add Supabase middleware to upload files
    Replace multer diskStorage with multer memoryStorage as files are no longer
    stored to disk. Files are now temporarily held in memoryStorage then
    passed to the Supabase middleware to upload in a Supabase bucket

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -3,20 +3,45 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
 const requireAuth = require("../middleware/requireAuth");
 const multer = require("multer");
-const upload = multer({ dest: "public/uploads/" });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const supabase = require("../config/supabase");
+const standardizeFilePath = require("../middleware/standardizeFilePath");
 
 router.get("/", requireAuth, dashboardController.renderDashboard);
 
 router.post(
   "/upload-file",
   upload.single("uploadedFile"),
-  dashboardController.addFile,
+  standardizeFilePath,
+  async function uploadFile(req, res) {
+    const { data, error } = await supabase.storage
+      .from("uploads")
+      .upload(req.filePath, req.file);
+    if (error) {
+      console.error(error);
+    } else {
+      req.file.filePath = data.path;
+      dashboardController.addFile(req, res);
+    }
+  },
 );
 
 router.post(
   "/upload-file/folder/:id",
   upload.single("uploadedFile"),
-  dashboardController.addFileToFolder,
+  standardizeFilePath,
+  async function uploadFile(req, res) {
+    const { data, error } = await supabase.storage
+      .from("uploads")
+      .upload(req.filePath, req.file);
+    if (error) {
+      console.error(error);
+    } else {
+      req.file.filePath = data.path;
+      dashboardController.addFileToFolder(req, res);
+    }
+  },
 );
 
 router.post("/create-folder", dashboardController.addNewFolder);
